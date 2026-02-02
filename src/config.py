af596117411ec04b78600c07cb2eaee2b66f658f
quiet-arthur <arthurhenricalado09@gmail.com>
2026-02-02 18:08:50 -0300

    feat: initial structure and project setup

--- src/config.py
+++ src/config.py
@@ -1,9 +1,38 @@
 # ====== ALMAH URLS ====== 
 
-BASE_URL = "https://alphaassessoriams.almahcondos.com.br"
-LOGIN_ENDPOINT = "/SIS/EmpresaWS.asmx/SelecionarPorLoginSenha"
-ACCESS_ENDPOINT = "/novo_acesso.aspx"
-UNITS_EXPORT_ENDPOINT = "/CND/UnidadeCondominioWS.asmx/Exportar"
-UNITS_BILLS_EXPORT_ENDPOINT = "/FIN/ContasAReceberWS.asmx/GerarRelatorioInadimplenciaCondominioXLS"
+ENDPOINTS = {
+    "login": "/SIS/EmpresaWS.asmx/SelecionarPorLoginSenha",
+    "access": "/novo_acesso.aspx",
+    "units_export": "/CND/UnidadeCondominioWS.asmx/Exportar",
+    "units_bils_export": "/FIN/ContasAReceberWS.asmx/GerarRelatorioInadimplenciaCondominioXLS",
+}
+
+# ======================
+
+# ====== ADMS CONFIG =====
+
+ADMS_CONFIG = {
+    "alpha": {
+        "url": "https://alphaassessoriams.almahcondos.com.br",
+        "ids_ambiente": {
+            "id_usuario": 341,
+            "id_estabelecimento": 1,
+            "id_perfil_de_uso": 1,
+        },
+        "user_login": "LOGIN_USER",
+        "user_password": "ALPHA_PASSWORD_HASH"
+    },
+
+    "expresso": {
+        "url": "https://expressoadmapp.almahcondos.com.br",
+        "ids_ambiente": {
+            "id_usuario": 6,
+            "id_estabelecimento": 1,
+            "id_perfil_de_uso": 1,
+        },
+        "user_login": "LOGIN_USER",
+        "user_password": "EXPRESSO_PASSWORD_HASH"
+    }
+}
 
 # ======================
--- src/scraper.py
+++ src/scraper.py
@@ -1,10 +1,35 @@
 import os
+import httpx
 
 from dotenv import load_dotenv
-from config import BASE_URL
+from config import ADMS_CONFIG, ENDPOINTS
 
 load_dotenv()
 
-print(BASE_URL)
-print(os.getenv("LOGIN_USER"))
-print(os.getenv("ALPHA_PASSWORD_HASH"))
+
+class AlmahScraper:
+    """
+    Handles authentication and data extraction from the Almah Web System.
+    """
+    def __init__(self, condom_id: str, adm: str):
+        """_summary_
+
+        Args:
+            condom_id (str): _description_
+        """ 
+        if adm not in ADMS_CONFIG:
+            raise ValueError(f"Administradora '{adm}' não encontrada nas configurações")
+
+        self.config = ADMS_CONFIG[adm]
+        self.ambiente = self.config["ids_ambiente"]
+
+        self.user = os.getenv(self.config["user_login"])
+        self.password = os.getenv(self.config["user_password"])
+
+def main():
+    AlphaClient = AlmahScraper("123", "alpha")
+
+    print(AlphaClient.config)
+
+
+main()
